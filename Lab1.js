da61630979ed6b1c48169a8ddc325c047a375af8
Gavin Hearn <ghearn117@gmail.com>
2024-08-29 10:10:45 -0400

    Lab 1 files

---
+++ Lab1.js
@@ -0,0 +1,19 @@
+function Lab1(){
+	var sum = 0;
+	var array = [];
+	for(var i = 0; i < 5; i++){
+		var rand = Math.random();
+		rand *= 100;
+		rand = Math.floor(rand);
+		sum += rand;
+		array.push(rand);
+	}
+	var mean = sum/array.length;
+	var greater = []
+	for(var i = 0; i < 5; i++){
+		if(array[i] > mean){
+			greater.push(array[i]);
+		}
+	}
+	document.querySelector("#arrayOut").innerHTML="The array is:"+array+"<br>The mean is:"+mean+"<br>Greater:"+greater;
+}
