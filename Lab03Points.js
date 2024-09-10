9d860455c0e50dc4ffd596a04cf391277edf28e0
Gavin Hearn <ghearn117@gmail.com>
2024-09-10 10:02:21 -0400

    Update Lab03Points.js

--- Lab03Points.js
+++ Lab03Points.js
@@ -11,8 +11,8 @@ function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
     points=[
-      vec2(0, 1),
-      vec2(1, 0),
+      vec2(1, 1),
+      vec2(0, 0),
       vec2(-1, -1)
     ];
     
