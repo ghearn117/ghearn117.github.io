4894351c195abdc126853caf00dd119e6c2ac08f
Gavin Hearn <ghearn117@gmail.com>
2024-09-09 20:48:01 -0400

    Update Lab1.js

--- Lab1.js
+++ Lab1.js
@@ -9,11 +9,12 @@ function Lab1(){
 		array.push(rand);
 	}
 	var mean = sum/array.length;
+	console.log(mean);
 	var greater = []
 	for(var i = 0; i < 5; i++){
 		if(array[i] > mean){
 			greater.push(array[i]);
 		}
 	}
-	document.querySelector("#Lab1").innerHTML="The array is:"+array+"<br>The mean is:"+mean+"<br>Greater:"+greater;
+	document.querySelector("#arrayOut").innerHTML="The array is:"+array+"<br>The mean is:"+mean+"<br>Greater:"+greater;
 }
