ece353e4f4e8327c6a9eb776568c8ed7158ad4ed
Gavin Hearn <ghearn117@gmail.com>
2024-11-14 09:48:54 -0500

    Update Lab17TextureSettings.js

--- Lab17TextureSettings.js
+++ Lab17TextureSettings.js
@@ -17,9 +17,9 @@ function init()
 
 // isoceles triangle
     points=[
-    vec4(  0.0, .5, 0.0, 1.0 ),
-    vec4(  .5, 0.0, 0.0, 1.0 ),
-    vec4(   1.0, 0.0, 0.0, 1.0 )
+    vec4(  0.0, 0.0, 0.0, 1.0 ),
+    vec4(  .5, -.5, 0.0, 1.0 ),
+    vec4(   -.5, -.5, 0.0, 1.0 )
     ];
 
     positionsArray.push(points[0]);
